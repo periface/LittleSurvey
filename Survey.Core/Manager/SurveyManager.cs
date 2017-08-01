5fed593e5561573e3b2a126187af81e0e5bb0b69
Alan Torres <temp_peri@hotmail.com>
2017-08-01 15:43:48 -0500

    Initial

--- Survey.Core/Manager/SurveyManager.cs
+++ Survey.Core/Manager/SurveyManager.cs
@@ -1,7 +1,78 @@
-﻿namespace Survey.Core.Manager
+﻿using System.Threading.Tasks;
+using Survey.Core.Entities;
+
+namespace Survey.Core.Manager
 {
     public class SurveyManager : ISurveyManager
     {
+        public Task CreateSurveyAsync(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public void CreateSurvey(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task EditSurveyAsync(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditSurvey(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteSurveyAsync(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteSurvey(Entities.Survey survey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddQuestionAsync(int questionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddQuestionAsync(Question question)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddQuestion(int questionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddQuestion(Question question)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveQuestionAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveQuestionAsync(Question question)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveQuestion(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveQuestion(Question question)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
