6226bf8882b2b1a6e20e7c915f291fae59e9ecb3
Alan Torres <temp_peri@hotmail.com>
2017-08-04 15:39:12 -0500

    plugin and update

---
+++ LittleSurvey.Web/Scripts/littlesurvey/littlesurvey.js
@@ -0,0 +1,27 @@
+﻿(function (container, surveyId, config) {
+    if (!config) config = {};
+    config.answerBtn = ".js-answer";
+    var $container = $("#container");
+
+    var $body = $("body");
+
+    $body.on("click",config.answerBtn,answerQuestion);
+
+    function answerQuestion() {
+
+        var $self = $(this);
+
+        var questionId = $self.data("questionid"), answer = $self.data("answer");
+
+        
+
+        loadNextQuestion();
+    }
+    function loadNextQuestion() {
+
+    }
+
+    return {
+        answerQuestion
+    }
+});
--- Survey.Application/SurveyService/ISurveyAppService.cs
+++ Survey.Application/SurveyService/ISurveyAppService.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections.Generic;
 using Abp.Application.Services;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Survey.Application.SurveyService.Dto;
 
 namespace Survey.Application.SurveyService
 {
     public interface ISurveyAppService : IApplicationService
     {
+        [HttpPost]
         Task<int> CreateEditSurvey(SurveyInputDto input);
+        [HttpPost]
         Task<int> CreateQuestion(QuestionInputDto input);
+        [HttpPut]
         Task AssignQuestionToSurvey(int surveyId, int questionId);
+        [HttpDelete]
         Task RemoveQuestionFromSurvey(int surveyId, int questionId);
+        [HttpGet]
         Task<SurveyForUserDto> GetSurvey(string url);
+        [HttpGet]
         Task<List<SurveyDto>> GetSurveys();
+        [HttpPost]
         Task<int> AddPredefinedAnswer(string txt);
+        [HttpPut]
         Task SetOfferedAnswer(int surveyId, int questionId, int idAnswer);
+        [HttpPost]
         Task AnswerQuestion(AnswerInputDto answer);
-
+        [HttpGet]
         QuestionDto GetQuestion(int surveyId, int questionId);
-
+        [HttpPost]
         Task BulkAnswer(List<AnswerInputDto> answers);
-
-
-
     }
 }
